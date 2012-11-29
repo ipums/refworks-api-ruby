068998b8f18f2bd95b8bebb4cef75c90321878db
Fran Fabrizio <fran@umn.edu>
2012-11-28 21:46:53 -0600

    Working end to end for newsess call

---
+++ lib/RefWorks/Authentication/authentication_request.rb
@@ -0,0 +1,3 @@
+class AuthenticationRequest
+  # To change this template use File | Settings | File Templates.
+end
---
+++ lib/RefWorks/Authentication/newsess/newsess_request.rb
@@ -0,0 +1,3 @@
+class NewsessRequest
+  # To change this template use File | Settings | File Templates.
+end
