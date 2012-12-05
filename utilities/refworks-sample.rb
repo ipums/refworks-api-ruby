1e06b07e3a7b916a8e45dd2a5741a0651b664a37
Fran Fabrizio <fran@umn.edu>
2012-12-05 16:35:24 -0600

    Poor man's testing.

--- utilities/refworks-sample.rb
+++ utilities/refworks-sample.rb
@@ -3,21 +3,15 @@ require './config/config.rb'
 
 rwc = Refworks.new(RefWorks::Config::CONFIG)
 
-#response = rwc.request(:class_name => 'authentication',
-#                       :method_name => 'newsess',
-#                       :method_params => {
-#                         :login_name => rwc.login_name(),
-#                         :group_code => rwc.group_code(),
-#                         :password => rwc.password(),
-#                       }
-#                      )
-
 response = rwc.request(:class_name => 'retrieve',
                        :method_name => 'quick',
                        :method_params => {
-                         :search => "Henderson"
+                         :search => "Jones",
+                         :pgsize => 10,
+                         :pgnum => 3,
                        }
 )
 
+pp "Received " + response.total_hits + " hits, " + response.total_returned + " of which were returned."
 pp response.references
 
