b9a83cb4c7799a75607928d61ca32801200d8484
Fran Fabrizio <fran@umn.edu>
2012-12-05 16:34:24 -0600

    Handling cases where there are 1 vs. more than 1 reference returned.

--- lib/refworks/retrieve/quick/retrieve_quick_response.rb
+++ lib/refworks/retrieve/quick/retrieve_quick_response.rb
@@ -1,13 +1,21 @@
 class RetrieveQuickResponse < RetrieveResponse
   attr_reader :references
 
-  def initialize(params, raw_response)
-    super(params, raw_response)
+  def initialize(raw_response)
+    super(raw_response)
     refs = self.parsed_response["refworks"]["RWResult"]["RetrieveResult"]["reference"]
     # here we parse out references into actual Reference objects
     @references = Array.new
-    refs.each do |ref|
-      @references << Reference.new(ref)
+
+    # As usual with RefWorks API, it can return an array or a single element depending on how many references were returned.
+    if (refs.class == Array)
+      refs.each do |ref|
+        @references << Reference.new(ref)
+      end
+    else
+      # here, "refs" is just a hash representing a single reference
+      # in other words, only one reference was returned
+      @references << Reference.new(refs)
     end
   end
 end
