0244bcaea8bba6d74d53cd3626ba6a48d5e50f6f
Fran Fabrizio <fran@umn.edu>
2012-12-14 13:13:46 -0600

    More project refactoring and cleanup.

--- utilities/test.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-        $expires = 1354085792000; // Poor man's milliseconds.
-        $signature = urlencode(base64_encode(hash_hmac('sha1', "authentication***REMOVED***1354085792000", "***REMOVED***", TRUE)));
-        print $signature . "\n";
-?>
