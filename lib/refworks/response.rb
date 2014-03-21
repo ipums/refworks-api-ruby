687716bb747501a183a49de68b41e95e2e828535
Fran Fabrizio <fran@umn.edu>
2014-03-21 10:47:13 -0500

    Backing out the transformations I was experimenting with to workaround the malformed error message bug.

--- lib/refworks/response.rb
+++ lib/refworks/response.rb
@@ -5,14 +5,13 @@ class Response
   def initialize(raw_response)
     # The RefWorks API emits invalid XML.  Specifically, when the session is invalid, the ResultMsg field
     # which comes back contains some illegal raw characters.  This is the only set of transformations I
-    # could find which completely eliminate the junk characters.
+    # could find which completely eliminate the junk characters, but it may be breaking other things.
 
-    p raw_response.body
-
-    raw_response.body.encode!('UTF-8', 'binary', :invalid => :replace, :undef => :replace, :replace => '?')
-    raw_response.body.gsub!(/[\u0001-\u0019]/,'?')
-
-    p raw_response.body
+    # Trying to work around the malformed response bug
+    #p raw_response.body
+    #raw_response.body.encode!('UTF-8', 'binary', :invalid => :replace, :undef => :replace, :replace => '?')
+    #raw_response.body.gsub!(/[\u0001-\u0019]/,'?')
+    #p raw_response.body
 
     @body = raw_response.body
     @parsed_response = raw_response.parsed_response
