2e85f64f549cd16d977c4e297239f6bd1a08c36f
Fran Fabrizio <fran@umn.edu>
2016-09-16 17:09:43 -0500

    Cleansing.

--- examples/simplified-bug-example.rb
+++ examples/simplified-bug-example.rb
@@ -10,9 +10,9 @@ expires = Time.now.to_i * 1000
 
 # RW formula for signature message
 # call class + access key + expires.to_s.chomp
-signature = 'retrieve' + '***REMOVED***' + expires.to_s.chomp
-hmacsig = OpenSSL::HMAC.digest('sha1', '***REMOVED***', signature)
+signature = 'retrieve' + 'ACCESS_KEY_HERE' + expires.to_s.chomp
+hmacsig = OpenSSL::HMAC.digest('sha1', 'HMAC_SIG_THING_HERE', signature)
 encodedsig = Base64.strict_encode64(hmacsig)
 
-response = Refworks.get("http://www.refworks.com/accessrw/?class=retrieve&pgsize=50&pgnum=1&format=html&sort=&style=0&language=en&biblist=false&method=author&search=Rhode%2CPaul+W.&folder=testfolder&signature=#{encodedsig}&accesskeyid=h83KW875%5Eousl&expires=#{expires}&sess=___BADSESSKEY___");
+response = Refworks.get("http://www.refworks.com/accessrw/?class=retrieve&pgsize=50&pgnum=1&format=html&sort=&style=0&language=en&biblist=false&method=author&search=Rhode%2CPaul+W.&folder=testfolder&signature=#{encodedsig}&accesskeyid=ACCESS_KEY_HERE&expires=#{expires}&sess=___BADSESSKEY___");
 p response
